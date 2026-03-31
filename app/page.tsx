ed5b1706a3e3bb50f97c35c6ab73e0a7438ddd31
xkalf0710 <mnymsuren37@gmail.com>
2026-03-31 12:25:14 +0800

    add1

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
    <>
-   <div>
-    Hello world
-   </div>
+   
+    <h1 className="text-3xl font-bold">
+       Hello world!
+    </h1>
+  
    </>
   );
 }
